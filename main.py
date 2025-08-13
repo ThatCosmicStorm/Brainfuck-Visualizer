59df88456d3fa3d9cdd1641421477d3847e580f7
ThatCosmicStorm <astronomerr2@gmail.com>
2025-08-13 02:25:22 -0400

    Create visualizer
    Arguments: script, Brainfuck program, delay (sec)

---
+++ main.py
@@ -0,0 +1,122 @@
+"""*Provides real-time visualization of a running Brainfuck program.*
+"""
+
+import sys
+from typing import List, Dict
+from readchar import readchar
+from tabulate import tabulate
+from time import sleep
+
+COMMANDS = {
+    "+", "-",
+    "<", ">",
+    "[", "]",
+    ".", ",",
+}
+
+
+def clear_screen():
+    print("\033[2J\033[H", end="")
+
+
+class Brainfuck:
+
+    def __init__(self):
+        self.stack: List[int] = []
+        self.bracemap: Dict[int, int] = {}
+        self.start = 0
+
+        self.array = {0: 0}
+        self.dp = 0
+        self.ip: int = 0
+
+        self.words: List[str] = []
+        self.output = False
+
+    def get_loop_positions(self, code: str):
+        for pos, cmd in enumerate(code):
+            match cmd:
+                case "[":
+                    self.stack.append(pos)
+                case "]":
+                    self.start = self.stack.pop()
+                    self.bracemap[self.start] = pos
+                    self.bracemap[pos] = self.start
+
+    def match(self, code: str):
+        match code[self.ip]:
+            case "+":
+                self.array[self.dp] = (self.array[self.dp] + 1) % 256
+            case "-":
+                self.array[self.dp] = (self.array[self.dp] - 1) % 256
+            case ">":
+                self.dp += 1
+                self.array.setdefault(self.dp, 0)
+            case "<":
+                if self.dp > 0:
+                    self.dp -= 1
+                else:
+                    exit("Error: Pointer moved left of 0")
+            case "[":
+                if not self.array[self.dp]:
+                    self.ip = self.bracemap[self.ip]
+            case "]":
+                if self.array[self.dp]:
+                    self.ip = self.bracemap[self.ip]
+            case ".":
+                self.output = True
+            case ",":
+                self.array[self.dp] = ord(readchar())
+
+        self.ip += 1
+
+    def update_table(self):
+        self.cells = [str(i) for i in self.array.keys()]
+        self.values = [
+            [str(i) for i in self.array.values()],
+            [ascii(chr(i)) for i in self.array.values()]
+        ]
+
+        self.cells[self.dp] = f"[ {self.cells[self.dp]} ]"
+
+        print(tabulate(self.values, headers=self.cells, tablefmt="grid"))
+
+    def execute(self, code: str, delay: int | float = 1):
+        self.get_loop_positions(code)
+
+        while self.ip < len(code):
+            clear_screen()
+            self.update_table()
+
+            if self.output:
+                self.words.append(chr(self.array[self.dp]))
+            self.output = False
+
+            sleep(delay)
+            self.match(code)
+
+        print("\n" + "Output:" + "\n")
+
+        for i, char in enumerate(self.words):
+            sys.stdout.write(char)
+
+
+def main():
+    if len(sys.argv) < 2:
+        exit(f"Usage: '{sys.argv[0]}' filename")
+
+    if len(sys.argv) > 2:
+        delay = float(sys.argv[2])
+    else:
+        delay = 1
+
+    try:
+        with open(sys.argv[1], "r", encoding="utf8") as f:
+            code = "".join(char for char in f.read() if char in COMMANDS)
+            Brainfuck().execute(code, delay=delay)
+    except FileNotFoundError:
+        exit(f"Error: File '{sys.argv[1]}' not found")
+
+
+if __name__ == "__main__":
+    main()
